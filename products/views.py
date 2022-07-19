0d62796b722ced1855f58691aa3886633a703462
Mohamed4x03 <muhammed.hassaan94@gmail.com>
2022-07-19 12:06:43 +0200

    inherit with dictionary

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
+from itertools import product
 from django.shortcuts import render
 from django.views.generic import ListView , DetailView
-from .models import Product 
+from .models import Product , ProductImage, Review
 # Create your views here.
 
 class ProductList(ListView):
@@ -12,6 +13,14 @@ class ProductList(ListView):
 class ProductDetail(DetailView):
     model = Product
     
+    
+    def get_context_data(self, **kwargs):
+        myproduct = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context["images"] =ProductImage.objects.filter(product=myproduct) 
+        context["Reviews"] =Review.objects.filter(product=myproduct)
+        return context
+      
     
 
 
