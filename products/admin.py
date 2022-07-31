814ead634278481ff8cdb71261e599241f731aae
Mohamed4x03 <muhammed.hassaan94@gmail.com>
2022-07-31 19:01:00 +0200

    add summernote

--- products/admin.py
+++ products/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from .models import Product , Review , Category , Brand , ProductImage
-
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
    
 
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(SummernoteModelAdmin):
     inlines = [ProductImageInline] 
+    summernote_fields = '__all__'
     
 
 admin.site.register(Product,ProductAdmin)
